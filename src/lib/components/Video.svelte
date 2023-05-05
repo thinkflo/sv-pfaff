d4b5cf58f26f5df1403e1dedbdb1d44ea9af73e4
HalCodes <hal@thinkflo.com>
2023-05-05 14:28:47 +0200

    Update Video

--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -1,4 +1,6 @@
-<video class="w-full" autoplay loop>
-  <source src="/video/pexels-junjun-alito-4161528-4096x2160-24fps.mp4" type="video/mp4">
+<video poster="/bilder/poster.jpg" class="w-full aspect-video" autoplay loop>
+  <source src="/video/pexels-junjun-alito-4161528-854x480-24fps-boomerrang.webm" type="video/webm">
+  <source src="/video/pexels-junjun-alito-4161528-854x480-24fps-boomerrang.mp4" type="video/mp4">
+  <track kind="captions">
 Your browser does not support the video tag.
 </video>
