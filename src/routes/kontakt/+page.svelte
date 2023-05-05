1eb6603f9f3cda69e7a521fa4762d8edc0a0cff9
HalCodes <hal@thinkflo.com>
2023-05-05 14:41:49 +0200

    Mobile fixes

--- src/routes/kontakt/+page.svelte
+++ src/routes/kontakt/+page.svelte
@@ -1,5 +1,5 @@
-<section class="flex px-24 mx-auto text-center  mx-auto w-full">
-  <div class="bg-gray-100 flex-1 flex flex-col justify-center space-y-4 text-sm text-gray-500">
+<section class="flex md:flex-row flex-col md:px-24 mx-auto text-center mx-auto w-full">
+  <div class="bg-gray-100 flex-1 flex flex-col justify-center space-y-4 text-sm text-gray-500 py-12 md:py-0">
     <h2 class="text-4xl uppercase text-gray-900">Konkakt</h2>
     <p>SV-Pfaff</p>
     <p>m.pfaff@sv-pfaff.de</p>
